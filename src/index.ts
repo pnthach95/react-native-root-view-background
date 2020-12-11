f847f11f8d03f04662227fbb1a0ce81f2aa9ecff
Pham Ngoc Thach <thachxyz123@gmail.com>
2020-12-11 15:31:39 +0700

    refactor: shorten name

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 import { NativeModules, Platform } from 'react-native';
 
 function hexToRgb(hex: string) {
@@ -16,24 +17,19 @@ function hexToRgb(hex: string) {
   throw new Error('Bad Hex');
 }
 
-function setRootViewBackgroundColorWithRGB(
-  r: number,
-  g: number,
-  b: number,
-  a: number,
-) {
-  NativeModules.RNRootViewBackground.setBackground(r, g, b, a);
+function setBackgroundRGB(r: number, g: number, b: number, a: number) {
+  NativeModules.ReactNativeRootViewBackground.setBackground(r, g, b, a);
 }
 
 function setRootViewBackgroundColor(hex: string) {
   if (Platform.OS === 'ios') {
     const [r, g, b] = hexToRgb(hex);
-    setRootViewBackgroundColorWithRGB(r, g, b, 1);
+    setBackgroundRGB(r, g, b, 1);
   } else {
     if (hex.length === 4) {
       hex = `#${hex[1]}${hex[1]}${hex[2]}${hex[2]}${hex[3]}${hex[3]}`;
     }
-    NativeModules.RNRootViewBackground.setBackground(hex);
+    NativeModules.ReactNativeRootViewBackground.setBackground(hex);
   }
 }
 
