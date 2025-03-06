05ac5e3764ab78dcd29edea49c0f6fc06110f730
Pham Ngoc Thach <thachxyz123@gmail.com>
2025-03-06 11:11:06 +0700

    feat: update example, clean code

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,5 @@
-import { NativeModules, Platform } from 'react-native';
+import RootViewBackground from './NativeRootViewBackground';
 import { colord } from 'colord';
-
-const LINKING_ERROR =
-  `The package '@pnthach95/react-native-root-view-background' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-// @ts-expect-error
-const isTurboModuleEnabled = global.__turboModuleProxy != null;
-
-const RootViewBackgroundModule = isTurboModuleEnabled
-  ? require('./NativeRootViewBackground').default
-  : NativeModules.RootViewBackground;
-
-const RootViewBackground = RootViewBackgroundModule
-  ? RootViewBackgroundModule
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
 
 export function setRootViewBackgroundColor(color: string) {
   const parsedColor = colord(color);
